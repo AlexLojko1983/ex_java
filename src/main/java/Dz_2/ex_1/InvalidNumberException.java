b064efbfad3e4c2c0866c7c6187f0255c16ec39f
AlexLojko <a_l_g@tut.by>
2023-08-31 21:23:12 +0300

    add 1 ex

---
+++ src/main/java/Dz_2/ex_1/InvalidNumberException.java
@@ -0,0 +1,9 @@
+package Dz_2.ex_1;
+
+public class InvalidNumberException extends Exception {
+    public InvalidNumberException(String message) {
+        super(message);
+    }
+
+
+}
---
+++ src/main/java/Dz_2/ex_1/Main.java
@@ -0,0 +1,15 @@
+package Dz_2.ex_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InvalidNumberException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number: ");
+        int a = Integer.parseInt(scanner.next());
+        if (a <= 0) {
+            throw new InvalidNumberException("Некорректное число");
+        }
+        System.out.println("Число корректно");
+    }
+}
